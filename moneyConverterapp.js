7362b2bc6957542f09deb043e8baaa4f76e944b9
Juank060790 <juank060790@gmail.com>
2020-07-06 23:25:55 +0700

    Money Converter app

---
+++ moneyConverterapp.js
@@ -0,0 +1,48 @@
+const exchange = 23208
+let currency = prompt("Change to VND or USD?")
+// let amount = prompt("How much?")
+
+function vndToUsd(amountVnd) {
+  amount = (amountVnd / exchange)
+  return amount.toFixed(2)
+}
+
+function usdToVnd(amountUsd) {
+  amount = (amountUsd * exchange)
+  return amount.toFixed(2)
+}
+
+function formatCurrency(type, value) {
+  const formatter = new Intl.NumberFormat(type, {
+    currency: type,
+    style: "currency"
+  });
+  return formatter.format(value);
+}
+
+if (currency.toLowerCase() === "vnd") {
+  let amountUsd = prompt("How much USD do you have?")
+
+  if (isNaN(amountUsd) === false){
+     amountUsd = (usdToVnd(amountUsd)) 
+     console.log(formatCurrency('vnd', amountUsd))
+  }
+  else {
+    console.log ("Please enter a number")
+  }
+}
+else if(currency.toLowerCase() === 'usd') {
+  let amountVnd = prompt("How much VND do you have?")
+ if (isNaN (amountVnd)=== false){
+    amountVnd = (vndToUsd(amountVnd))
+    console.log(formatCurrency('usd',amountVnd))
+ }
+ else {
+    console.log ("Please enter a number")
+  }
+}
+else {
+  console.log ('Please choose VND or USD')
+}
+
+
